069dc4ee45dfeb39cc8b10ae314f2cd3dff22ea0
lamnhan28081996 <lamnhan28081996@gmail.com>
2018-06-14 09:02:31 +0700

    Tới phần cart cơ bản

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -6,4 +6,15 @@ class Product < ApplicationRecord
         :with => %r{\.(gif|jpg|png)\Z}i,
         :message => 'Chi nhan file GIF, JPG, PNG'
     }
+    has_many :line_items
+    before_destroy :check_if_has_line_item
+	private
+  	def check_if_has_line_item
+        if line_items.empty?
+            return true
+        else
+            errors.add(:base, 'This product has a LineItem')
+            return false
+        end
+    end
 end
